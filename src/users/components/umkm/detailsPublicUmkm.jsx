6a660edecd066daffe2e375c534bdc64400e4b97
Sudarmo Adi Joyo Putro <134109668+Venc1el@users.noreply.github.com>
2023-09-24 07:30:29 +0700

    Update detailsPublicUmkm.jsx

--- src/users/components/umkm/detailsPublicUmkm.jsx
+++ src/users/components/umkm/detailsPublicUmkm.jsx
@@ -21,7 +21,7 @@ function DetailsUmkm() {
                     <ol className="flex items-center space-x-1 md:space-x-3">
                         <li className="inline-flex items-center">
                             <Link
-                                to="/syscon"
+                                to="/"
                                 className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
                             >
                                 <svg
@@ -56,7 +56,7 @@ function DetailsUmkm() {
                                     />
                                 </svg>
                                 <Link
-                                    to="/syscon/umkm"
+                                    to="/umkm"
                                     className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white"
                                 >
                                     Umkm
