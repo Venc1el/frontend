4502ef4cf886d218d9ba4027b5f0c7c47c260445
Sudarmo Adi Joyo Putro <134109668+Venc1el@users.noreply.github.com>
2023-09-27 02:37:26 +0700

    Update search

--- src/users/components/content/accordion.jsx
+++ src/users/components/content/accordion.jsx
@@ -92,12 +92,6 @@ function SearchForm({ handleSearch }) {
                     required
                     onChange={handleSearch} // Call the handleSearch function on input change
                 />
-                <button
-                    type="submit"
-                    className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                    Search
-                </button>
             </div>
         </form>
     );
