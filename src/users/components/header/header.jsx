68c5b5604f0e6673f9adf886cc630e10ca051f51
Sudarmo Adi Joyo Putro <134109668+Venc1el@users.noreply.github.com>
2023-09-27 08:31:07 +0700

    update link

--- src/users/components/header/header.jsx
+++ src/users/components/header/header.jsx
@@ -38,9 +38,9 @@ const Header = () => {
                             <button onClick={handleLogout}>Logout</button>
                         ) : (
 
-                            <a href="/login" className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-full text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
+                            <Link to = "/login" className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-full text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
                                 Log In
-                            </a>
+                            </Link>
                         )}
                         <button
                             data-collapse-toggle="mobile-menu-2"
