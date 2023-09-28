f1c9c4b239e188235f2ad7174ae5ee6394b5ddc9
Sudarmo Adi Joyo Putro <134109668+Venc1el@users.noreply.github.com>
2023-09-28 14:09:31 +0700

    update header

--- src/users/components/header/header.jsx
+++ src/users/components/header/header.jsx
@@ -78,7 +78,7 @@ const Header = () => {
                                 <Link
                                     to="/umkm"
                                     className={`block py-2 pr-4 pl-3 ${
-                                        location.pathname === '/umkm' ? 'text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white' : 'text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700'
+                                        location.pathname === '/umkm' ? 'text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white' : ''
                                     }`}
                                 >
                                     Umkm
