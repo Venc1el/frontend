b4013dae9ca82b21ad4d91c8725a1b3645409759
Sudarmo Adi Joyo Putro <134109668+Venc1el@users.noreply.github.com>
2023-09-28 14:08:03 +0700

    update header

--- src/users/components/header/header.jsx
+++ src/users/components/header/header.jsx
@@ -1,26 +1,26 @@
 import axios from "axios";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
     const storedEncryptedLevel = localStorage.getItem('encryptedUserLevel');
+    const location = useLocation();
 
-    axios.defaults.withCredentials = true
+    axios.defaults.withCredentials = true;
+
     const handleLogout = () => {
         axios.get('https://delightful-tan-scallop.cyclic.cloud/logout')
             .then(res => {
                 if (res.data.status === "Success") {
-                    localStorage.removeItem('encryptedUserLevel')
-                    window.location.reload()
+                    localStorage.removeItem('encryptedUserLevel');
+                    window.location.reload();
                 } else {
-                    alert('Error')
+                    alert('Error');
                 }
-            }).catch(err => console.error(err))
+            }).catch(err => console.error(err));
     };
 
-
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
 
     const toggleMobileMenu = () => {
         setIsMobileMenuOpen(!isMobileMenuOpen);
@@ -30,15 +30,17 @@ const Header = () => {
         <header style={{ borderBottom: '1px solid rgba(0 , 0 , 0 ,0.1)' }}>
             <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                    <Link to ="/" className="flex items-center">
+                    <Link to="/" className="flex items-center">
                         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Jambangan</span>
                     </Link>
                     <div className="flex items-center lg:order-2">
                         {storedEncryptedLevel ? (
                             <button onClick={handleLogout}>Logout</button>
                         ) : (
-
-                            <Link to = "/login" className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-full text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
+                            <Link
+                                to="/login"
+                                className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-full text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+                            >
                                 Log In
                             </Link>
                         )}
@@ -62,14 +64,22 @@ const Header = () => {
                     <div className={`justify-between items-center w-full lg:flex lg:w-auto lg:order-1 ${isMobileMenuOpen ? 'block' : 'hidden'}`} id="mobile-menu-2">
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
-                                <Link to='/' className="block py-2 pr-4 pl-3 ${activeLink === 'Home' ? text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">
+                                <Link
+                                    to="/"
+                                    className={`block py-2 pr-4 pl-3 ${
+                                        location.pathname === '/' ? 'text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white' : ''
+                                    }`}
+                                    aria-current="page"
+                                >
                                     Home
                                 </Link>
                             </li>
                             <li>
                                 <Link
-                                    to='/umkm'
-                                    className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                                    to="/umkm"
+                                    className={`block py-2 pr-4 pl-3 ${
+                                        location.pathname === '/umkm' ? 'text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white' : 'text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700'
+                                    }`}
                                 >
                                     Umkm
                                 </Link>
@@ -79,7 +89,6 @@ const Header = () => {
                 </div>
             </nav>
         </header>
-
     );
 };
 
