b4c5d8f9b98e70fff2a1abc2d6db3d3a22e6104d
Sudarmo Adi Joyo Putro <134109668+Venc1el@users.noreply.github.com>
2023-09-28 14:12:12 +0700

    Header update

--- src/users/components/header/header.jsx
+++ src/users/components/header/header.jsx
@@ -54,7 +54,7 @@ const Header = () => {
                         >
                             <span className="sr-only">Open main menu</span>
                             <svg className={`w-6 h-6 ${isMobileMenuOpen ? 'hidden' : ''}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
+                                <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12 a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
                             </svg>
                             <svg className={`w-6 h-6 ${isMobileMenuOpen ? '' : 'hidden'}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path>
@@ -66,9 +66,7 @@ const Header = () => {
                             <li>
                                 <Link
                                     to="/"
-                                    className={`block py-2 pr-4 pl-3 ${
-                                        location.pathname === '/' ? 'text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white' : ''
-                                    }`}
+                                    className={`block py-2 pr-4 pl-3 ${location.pathname === '/' ? 'text-primary-700 dark:text-white' : 'text-gray-700 dark:text-gray-400'}`}
                                     aria-current="page"
                                 >
                                     Home
@@ -77,9 +75,7 @@ const Header = () => {
                             <li>
                                 <Link
                                     to="/umkm"
-                                    className={`block py-2 pr-4 pl-3 ${
-                                        location.pathname === '/umkm' ? 'text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white' : ''
-                                    }`}
+                                    className={`block py-2 pr-4 pl-3 ${location.pathname === '/umkm' ? 'text-primary-700 dark:text-white' : 'text-gray-700 dark:text-gray-400'}`}
                                 >
                                     Umkm
                                 </Link>
