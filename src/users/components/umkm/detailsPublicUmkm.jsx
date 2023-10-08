0dcdd2bdccfac83d4bffacc0839bd7201f5a4be0
DarumaDaichi <sudarmoadi89@gmail.com>
2023-10-08 22:48:53 +0700

    Updating detail public Umkm

--- src/users/components/umkm/detailsPublicUmkm.jsx
+++ src/users/components/umkm/detailsPublicUmkm.jsx
@@ -12,56 +12,100 @@ function DetailsUmkm() {
                 setUmkm(response.data);
             })
             .catch((error) => {
-                console.error('Error fetching data:', error);
             });
     }, [postId]);
-
     return (
-        <div className="container mx-auto mt-8 p-8 bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700">
-            <nav className='my-5'>
-                <ol className="flex items-center space-x-2">
-                    <li className="inline-flex items-center text-gray-700 dark:text-gray-400 hover:text-blue-600">
-                        <Link to="/" className="flex items-center">
-                            <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="M2 10.39l7-7.6v4.72h4v6.96h-4v4.72l-7-7.6z" />
-                            </svg>
-                            Home
-                        </Link>
-                    </li>
-                    <li className="inline-flex items-center text-gray-700 dark:text-gray-400 hover:text-blue-600">
-                        <span className="flex items-center">
-                            <svg className="w-4 h-4 mx-2" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="M2 10.39l7-7.6v4.72h4v6.96h-4v4.72l-7-7.6z" />
-                            </svg>
-                            <Link to="/umkm">Umkm</Link>
-                        </span>
-                    </li>
-                    <li className="inline-flex items-center text-gray-700 dark:text-gray-400">
-                        <span className="flex items-center">
-                            <svg className="w-4 h-4 mx-2" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="M2 10.39l7-7.6v4.72h4v6.96h-4v4.72l-7-7.6z" />
-                            </svg>
-                            {umkm.judul}
-                        </span>
-                    </li>
-                </ol>
-            </nav>
-
-            {umkm.image && (
-                <img className="object-cover w-full h-60 rounded-t-lg" src={umkm.image} alt={umkm.judul} />
-            )}
-
-            <div className="mt-8">
-                <h1 className="text-3xl font-bold text-gray-900 dark:text-white">{umkm.judul}</h1>
-                <p className="mt-2 text-gray-700 dark:text-gray-400">{umkm.kategori}</p>
-                <div className="mt-4 text-gray-700 dark:text-gray-400">
-                    {umkm.content && umkm.content.split('\n').map((paragraph, index) => (
-                        <p key={index}>{paragraph}</p>
-                    ))}
+        <>
+            <div className="container px-10 bg-white border border-gray-200 rounded-lg shadow">
+                <nav aria-label="Breadcrumb" className='my-5 ml-5'>
+                    <ol className="flex items-center space-x-1 md:space-x-3">
+                        <li className="inline-flex items-center">
+                            <Link
+                                to="/"
+                                className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
+                            >
+                                <svg
+                                    className="w-3 h-3 mr-2.5"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 20 20"
+                                >
+                                    <path
+                                        d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"
+                                    />
+                                </svg>
+                                Dashboard
+                            </Link>
+                        </li>
+                        <li>
+                            <div className="flex items-center">
+                                <svg
+                                    className="w-3 h-3 text-gray-400 mx-1"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 6 10"
+                                >
+                                    <path
+                                        stroke="currentColor"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth={2}
+                                        d="m1 9 4-4-4-4"
+                                    />
+                                </svg>
+                                <Link
+                                    to="/umkm"
+                                    className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white"
+                                >
+                                    Umkm
+                                </Link>
+                            </div>
+                        </li>
+                        <li aria-current="page">
+                            <div className="flex items-center">
+                                <svg
+                                    className="w-3 h-3 text-gray-400 mx-1"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 6 10"
+                                >
+                                    <path
+                                        stroke="currentColor"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth={2}
+                                        d="m1 9 4-4-4-4"
+                                    />
+                                </svg>
+                                <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">
+                                    {umkm.judul}
+                                </span>
+                            </div>
+                        </li>
+                    </ol>
+                </nav>
+                {umkm.image && (
+                    <img className="object-cover w-max h-60 rounded-t-lg" src={`${umkm.image}`} alt="" />
+                )}
+                <div className="p-5">
+                    <h5 className="mb-2 text-xs text-gray-700 dark:text-white">
+                        {umkm.kategori}
+                    </h5>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        {umkm.judul}
+                    </h5>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        {umkm.content}
+                    </p>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        {umkm.alamat}
+                    </p>
                 </div>
-                <p className="mt-4 text-gray-700 dark:text-gray-400">{umkm.alamat}</p>
             </div>
-        </div>
+        </>
     );
 }
 
